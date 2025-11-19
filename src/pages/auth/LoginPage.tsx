7ea95c1bf69a25a02729714472956f9c310a200d
superdean55 <superdean55@gmail.com>
2025-11-19 20:19:24 +0100

    feat(auth): add LoginPage with RHF, AuthCard, Input, Button and server-side error handling
    - Implemented LoginPage using React Hook Form without client-side validation.
    - Added global error handling using  for server responses.
    - Integrated custom UI components: AuthCard, Input, and Button.
    - Added link to Register page for users without an account.

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,11 +1,84 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
+import AuthCard from "../../components/ui/AuthCard";
+import Input from "../../components/ui/Input";
+import Button from "../../components/ui/Button";
+
+type LoginData = {
+  email: string;
+  password: string;
+};
+
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginData>({
+    mode: "onSubmit",
+    defaultValues: { email: "", password: "" },
+  });
+
+  const onSubmit: SubmitHandler<LoginData> = async (data) => {
+    try {
+      await new Promise((r) => setTimeout(r, 800));
+      
+      // for testing purposes only
+      const isInvalid =
+        data.email !== "demo@example.com" || data.password !== "password";
+      if (isInvalid) {
+        setError("root", {
+          type: "server",
+          message: "Invalid email or password",
+        });
+        return;
+      }
+      console.log("Login successful:", data);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      setError("root", {
+        type: "server",
+        message: "Internal server error",
+      });
+    }
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md text-center">
-        <h1 className="text-2xl font-bold mb-4 text-gray-700">Login Page</h1>
-        <p className="text-gray-600">Route: /login</p>
-        {/* to do */}
-      </div>
-    </div>
+    <AuthCard>
+      <h1 className="text-2xl text-gray-600 font-bold mb-4 text-center">Login</h1>
+
+      {errors.root && (
+        <p className="text-red-500 text-sm mb-4 text-center">
+          {errors.root.message}
+        </p>
+      )}
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Enter your email"
+          {...register("email")}
+        />
+
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          {...register("password")}
+        />
+
+        <Button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Loading..." : "Login"}
+        </Button>
+      </form>
+
+      <p className="pt-4 text-sm text-gray-600 text-center">
+        Don't have an account?{" "}
+        <a href="/register" className="text-blue-600 hover:underline">
+          Register
+        </a>
+      </p>
+    </AuthCard>
   );
 }
